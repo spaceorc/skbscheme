6c96b24b97d8bb94372d9dffcf72b57b4af56ac9
spaceorc@yandex.ru <spaceorc@yandex.ru@c2d73e71-19ff-4ea8-0d92-7015634325e8>
2010-06-23 15:43:27 +0000

    started: lazy function "cond"

--- trunk/Engine/Redex.c
+++ trunk/Engine/Redex.c
@@ -11,8 +11,8 @@
 
 ConstantStr globalFunctionNames [] = {"+", "-", "=", "cons", "car", "cdr", "error"};
 FunctionPtr globalFunctionPointers [] = {OperatorPlus, OperatorMinus, OperatorNumberEq, FunctionCons, FunctionCar, FunctionCdr, FunctionError};
-ConstantStr globalLazyFunctionNames [] = {"let", "define", "lambda", "and", "or", "if"};
-LazyFunctionPtr globalLazyFunctionPointers [] = {LazyFunctionLet, LazyFunctionDefine, LazyFunctionLambda, LazyFunctionAnd, LazyFunctionOr, LazyFunctionIf};
+ConstantStr globalLazyFunctionNames [] = {"let", "define", "lambda", "and", "or", "if", "cond"};
+LazyFunctionPtr globalLazyFunctionPointers [] = {LazyFunctionLet, LazyFunctionDefine, LazyFunctionLambda, LazyFunctionAnd, LazyFunctionOr, LazyFunctionIf, LazyFunctionCond};
 ConstantStr globalConstantNames [] = {"#t", "#f", "null"};
 CreateConstantPtr globalConstantFunctionPointers [] = {True, False, Nil};
 
