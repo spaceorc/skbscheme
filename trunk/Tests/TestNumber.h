dcbcaae3340e26d97172bbbede77523ebd2a0a54
lspliner <lspliner@c2d73e71-19ff-4ea8-0d92-7015634325e8>
2010-07-22 12:03:35 +0000

    refactor

--- trunk/Tests/TestNumber.h
+++ trunk/Tests/TestNumber.h
@@ -1,71 +1,74 @@
 #include "Test.h"
 
 TEST(NumberPlus) {
-	Term * z = OperatorPlus(MakeList(2, Number(1), Number(2)));
-    AssertThat((z->tag == terNumber) && (z->number == 3));
+	ContextBindings * contextBindings = AcquireContextBindings();
+	AssertEq(Number(3), Eval(ParseSingle("(+ 1 2)"), contextBindings));
 }
 
 TEST(NumberPlusNoArguments) {
-	Term * z = OperatorPlus(MakeList(0));
-    AssertThat(z->tag == terError);
+	ContextBindings * contextBindings = AcquireContextBindings();
+	AssertEq(InvalidArgumentCount(), Eval(ParseSingle("(+)"), contextBindings));
 }
 
 TEST(NumberPlus3Arguments) {
-	Term * z = OperatorPlus(MakeList(3, Number(1), Number(2), Number(3)));
-    AssertThat((z->tag == terNumber) && (z->number == 6));
+	ContextBindings * contextBindings = AcquireContextBindings();
+	AssertEq(Number(6), Eval(ParseSingle("(+ 1 2 3)"), contextBindings));
 }
 
 TEST(NumberPlus1Argument) {
-	Term * z = OperatorPlus(MakeList(1, Number(1)));
-    AssertThat(z->tag == terError);
+	ContextBindings * contextBindings = AcquireContextBindings();
+	AssertEq(InvalidArgumentCount(), Eval(ParseSingle("(+ 1)"), contextBindings));
 }
 
 TEST(NumberPlus1stIsNotANumber) {
-	Term * z = OperatorPlus(MakeList(1, Nil()));
-    AssertThat(z->tag == terError);
+	ContextBindings * contextBindings = AcquireContextBindings();
+	AssertEq(InvalidArgumentType(), Eval(ParseSingle("(+ #\\c 1)"), contextBindings));
 }
 
 TEST(NumberPlus3Arguments2ndIsNotANumber) {
-	Term * z = OperatorPlus(MakeList(3, Number(1), Nil(), Number(3)));
-    AssertThat(z->tag == terError);
+	ContextBindings * contextBindings = AcquireContextBindings();
+	AssertEq(InvalidArgumentType(), Eval(ParseSingle("(+ 1 #\\c 2)"), contextBindings));
 }
 
 TEST(NumberMinus) {
-	Term * z = OperatorMinus(MakeList(2, Number(5), Number(2)));
-    AssertThat((z->tag == terNumber) && (z->number == 3));
+	ContextBindings * contextBindings = AcquireContextBindings();
+	AssertEq(Number(3), Eval(ParseSingle("(- 5 2)"), contextBindings));
 }
 
 TEST(NumberMinusNoArguments) {
-	Term * z = OperatorMinus(MakeList(0));
-    AssertThat(z->tag == terError);
+	ContextBindings * contextBindings = AcquireContextBindings();
+	AssertEq(InvalidArgumentCount(), Eval(ParseSingle("(-)"), contextBindings));
 }
 
 TEST(NumberMinus1Argument) {
-	Term * z = OperatorMinus(MakeList(1, Number(1)));
-    AssertThat((z->tag == terNumber) && (z->number == -1));
+	ContextBindings * contextBindings = AcquireContextBindings();
+	AssertEq(Number(-1), Eval(ParseSingle("(- 1)"), contextBindings));
 }
 
 TEST(NumberMinus3Arguments) {
-	Term * z = OperatorMinus(MakeList(3, Number(1), Number(2), Number(3)));
-    AssertThat((z->tag == terNumber) && (z->number == -4));
+	ContextBindings * contextBindings = AcquireContextBindings();
+	AssertEq(Number(-4), Eval(ParseSingle("(- 1 2 3)"), contextBindings));
 }
 
 TEST(NumberMinus1stIsNotANumber) {
-	Term * z = OperatorMinus(MakeList(1, Nil()));
-    AssertThat(z->tag == terError);
+	ContextBindings * contextBindings = AcquireContextBindings();
+	AssertEq(InvalidArgumentType(), Eval(ParseSingle("(- #\\c 1)"), contextBindings));
 }
 
 TEST(NumberEq) {
-	AssertEq(True(), OperatorNumberEq(MakeList(3, Number(1), Number(1), Number(1))));
-	AssertEq(False(), OperatorNumberEq(MakeList(3, Number(1), Number(1), Number(10))));
+	ContextBindings * contextBindings = AcquireContextBindings();
+	AssertEq(True(), Eval(ParseSingle("(= 1 1 1)"), contextBindings));
+	AssertEq(False(), Eval(ParseSingle("(= 1 1 2)"), contextBindings));
 }
 
 TEST(NumberEqWith1Argument) {
-	AssertEq(InvalidArgumentCount(), OperatorNumberEq(MakeList(1, Number(1))));
+	ContextBindings * contextBindings = AcquireContextBindings();
+	AssertEq(InvalidArgumentCount(), Eval(ParseSingle("(= 1)"), contextBindings));
 }
 
 TEST(NumberEqWithNotANumber) {
-	AssertEq(InvalidArgumentType(), OperatorNumberEq(MakeList(3, Number(1), Number(2), Nil())));
+	ContextBindings * contextBindings = AcquireContextBindings();
+	AssertEq(InvalidArgumentType(), Eval(ParseSingle("(= 1 2 #\\c)"), contextBindings));
 }
 
 TEST(EvalOfOperatorNumberEq) {
