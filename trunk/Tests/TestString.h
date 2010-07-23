96e67f8820d2000417fd70c7ef47d75bf032be76
lspliner <lspliner@c2d73e71-19ff-4ea8-0d92-7015634325e8>
2010-07-23 04:09:01 +0000

    string-set!, substring

--- trunk/Tests/TestString.h
+++ trunk/Tests/TestString.h
@@ -29,3 +29,28 @@ TEST(StringRef) {
 	AssertEq(ContractError(), Eval(ParseSingle("(string-ref \"abcdef\" (- 1))"), contextBindings));
 	AssertEq(ContractError(), Eval(ParseSingle("(string-ref \"abcdef\" 6)"), contextBindings));
 }
+
+TEST(StringSet) {
+	ContextBindings * contextBindings = AcquireContextBindings();
+	AssertEq(Empty(), Eval(ParseSingle("(define (lalala str k char) (string-set! str k char) str)"), contextBindings));
+	AssertEq(ConstantStringFromConstantStr(STR("abZdef")), Eval(ParseSingle("(lalala \"abcdef\" 2 #\\Z)"), contextBindings));
+	AssertEq(Empty(), Eval(ParseSingle("(string-set! \"abcdef\" 2 #\\Z)"), contextBindings));
+	AssertEq(InvalidArgumentCount(), Eval(ParseSingle("(string-set! \"abcdef\" 2)"), contextBindings));
+	AssertEq(InvalidArgumentType(), Eval(ParseSingle("(string-set! 2 \"abcdef\" #\\Z)"), contextBindings));
+	AssertEq(ContractError(), Eval(ParseSingle("(string-set! \"abcdef\" (- 1) \\Z)"), contextBindings));
+	AssertEq(ContractError(), Eval(ParseSingle("(string-set! \"abcdef\" 6 \\Z)"), contextBindings));
+}
+
+TEST(Substring) {
+	ContextBindings * contextBindings = AcquireContextBindings();
+	AssertEq(InvalidArgumentCount(), Eval(ParseSingle("(substring)"), contextBindings));
+	AssertEq(InvalidArgumentCount(), Eval(ParseSingle("(substring 1)"), contextBindings));
+	AssertEq(InvalidArgumentType(), Eval(ParseSingle("(substring 2 \"abcdef\")"), contextBindings));
+	AssertEq(ContractError(), Eval(ParseSingle("(substring \"abcdef\" (- 1))"), contextBindings));
+	AssertEq(ContractError(), Eval(ParseSingle("(substring \"abcdef\" 6)"), contextBindings));
+	AssertEq(ContractError(), Eval(ParseSingle("(substring \"abcdef\" 2 (- 1))"), contextBindings));
+	AssertEq(ContractError(), Eval(ParseSingle("(substring \"abcdef\" 2 1)"), contextBindings));
+	AssertEq(ContractError(), Eval(ParseSingle("(substring \"abcdef\" 2 6)"), contextBindings));
+	AssertEq(ConstantStringFromConstantStr(STR("cdef")), Eval(ParseSingle("(substring \"abcdef\" 2)"), contextBindings));
+	AssertEq(ConstantStringFromConstantStr(STR("cde")), Eval(ParseSingle("(substring \"abcdef\" 2 4)"), contextBindings));
+}
